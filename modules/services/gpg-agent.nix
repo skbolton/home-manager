3f5ef86b6af7e8fd8363e72df241b5b8e27ca113
Stephen Bolton <stephen@bitsonthemind.com>
2026-01-04 02:28:46 -0500

    Fix syntax error

--- modules/services/gpg-agent.nix
+++ modules/services/gpg-agent.nix
@@ -20,7 +20,7 @@ let
 
   gpgSshSupportStr = ''
     if [ -z "$SSH_CONNECTION" -o -z "$SSH_AUTH_SOCK" ] && [ "''${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-      "${gpgPkg}/bin/gpg-connect-agent --quiet updatestartuptty /bye";
+      ${gpgPkg}/bin/gpg-connect-agent --quiet updatestartuptty /bye
     fi
   '';
 
